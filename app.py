73095a02de1bbdc0368a249198ae779b0464bf60
abadari3 <abadari3@gatech.edu>
2020-05-09 02:46:32 -0400

    b

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from flask_heroku import Heroku
+# from flask_heroku import Heroku
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-heroku = Heroku(app)
+# heroku = Heroku(app)
 
 class Todo(db.Model):
     __tablename__ = "tasks"
