613f96d07113674c47130e94658f3d056fdced93
abadari3 <abadari3@gatech.edu>
2020-05-09 02:42:51 -0400

    postgres

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+from flask_heroku import Heroku
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+heroku = Heroku(app)
 
 class Todo(db.Model):
+    __tablename__ = "tasks"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable = False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
