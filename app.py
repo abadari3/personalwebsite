b732540d9a2ae59f00a85161abef453e8909e9c9
abadari3 <abadari3@gatech.edu>
2020-05-12 19:07:03 -0400

    just declaring database a total loss for now

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hpkzufpyrlpwsl:223744b81464f
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-with app.app_context:
-    db.create_all()
 
 @app.route('/')
 def index():
