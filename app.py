ebcb3798daec7d5e92d637b18f347da9ef2e1dbc
Ananda Badari <abadari3@gatech.edu>
2020-12-14 15:48:17 -0500

    reformat

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
-from models import *
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://hpkzufpyrlpwsl:223744b81464ff97c191afbd8f35565e58a4d47f64db56dba1fee1013f7b3867@ec2-3-231-16-122.compute-1.amazonaws.com:5432/de2o41b7f60dk8'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
 # test test
 
