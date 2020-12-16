e7cc4d140d6956b6acf97072064c42a890c831f9
anandabadari <45866670+anandabadari@users.noreply.github.com>
2020-12-15 21:48:08 -0500

    updates

--- app.py
+++ app.py
@@ -2,33 +2,29 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-# test test
-
 @app.route('/')
 def index():
-    # return todo()
-    # return redirect("https://www.linkedin.com/in/anandabadari/")
     return render_template('index.html')
 
-@app.route('/home')
-def home():
-    # return todo()
-    return render_template('index.html')
+@app.route('/aboutme')
+def about():
+    return render_template('about.html')
 
-@app.route('/cvproject')
-def cvproject():
-    # return todo()
-    return render_template('cvindex.html')
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
 
-@app.route('/cvproposal')
-def cvprop():
-    # return todo()
-    return render_template('cvprop.html')
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+    
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
-@app.route('/cvmidterm')
-def cvmidterm():
-    # return todo()
-    return render_template('cvmidterm.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 
 if __name__ == '__main__':
